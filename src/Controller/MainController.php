7578af473ca9892484adfb2f756f4fa140e6953b
bugloper <bugloper@gmail.com>
2025-01-12 18:05:00 +0600

    template inheritacnce

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,19 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<h1>Hello World!</h1>');
+        $noOfStarShips = 69.420;
+        return $this->render(
+            'main/homepage.html.twig',
+            ['noOfStarShips' => $noOfStarShips]
+        );
     }
 }
